9f1d9b83bfc423670039bdf35c97fc9b898ce377
Shilo <89766508+EspressoMusic@users.noreply.github.com>
2026-05-20 00:32:26 +0300

    Create config.js

---
+++ docs/config.js
@@ -0,0 +1,14 @@
+window.PEYMI_CONFIG = {
+  appName: "Bizmi",
+  publicBaseUrl: "https://espressomusic.github.io/PEYMI",
+  basePath: "/PEYMI",
+  supabaseUrl: "https://qruzhluqmmzlcxksftuh.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFydXpobHVxbW16bGN4a3NmdHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNzU3MTUsImV4cCI6MjA5NDc1MTcxNX0.K__lDNCH08kRVBt11jdu1rl5zxaRdTZ46g1jYHL5d1U",
+  testingMode: true,
+  apkDownloadUrl: "",
+  playStoreUrl: "",
+  appStoreUrl: "",
+  androidPackage: "com.example.bakery_shop_app",
+  deepLinkScheme: "bizmi",
+  tryOpenAppFirst: true
+};
